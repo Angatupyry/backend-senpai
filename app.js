7f6c88606b9b12550095d60ee2b3f70a2ae47439
Angatupyry <fierrofenix@gmail.com>
2021-11-11 19:03:53 -0300

    Se agregan rutas de login y de tarea

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRouter = require("./src/routes/auth");
+const { router: tareasRouter } = require("./src/routes/tarea");
+
 const app = express();
 const PORT = 3000;
 
@@ -16,6 +19,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hola");
 });
+
+app.use("/auth", authRouter);
+app.use("/tareas", tareasRouter);
 
 app.listen(PORT, function () {
   console.log(`Corriendo en el puerto ${PORT}`);
